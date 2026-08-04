86b296981fb0dcbf5fdddf63a98d319593e00e6d
Michael Rosenberg <michael@mrosenberg.pub>
2026-08-03 23:47:00 -0400

    Use keccak portable backend for serial benches

--- bench.sh
+++ bench.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+# Just does serial benches for now
+
+SERIAL_FLAGS='--cfg kopis_backend="serial" --cfg keccak_backend="soft"'
+
 # Alignment flags come from https://www.bazhenov.me/posts/2024-02-performance-roulette/
-RUSTFLAGS='--cfg kopis_backend="serial" -C llvm-args=-align-all-functions=6 -C llvm-args=-align-all-nofallthru-blocks=6 -C target-cpu=native' \
-    cargo bench $@
+PERF_FLAGS="-C llvm-args=-align-all-functions=6 -C llvm-args=-align-all-nofallthru-blocks=6 -C target-cpu=native"
+
+RUSTFLAGS="${SERIAL_FLAGS} ${PERF_FLAGS}" cargo bench $@
